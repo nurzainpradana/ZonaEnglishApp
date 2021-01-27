9e8eb3416924a9ea1005aaa45b8ad64d0dca590c
Hari Agus W <57583223+HariAgus@users.noreply.github.com>
2021-01-27 13:24:39 +0700

    create btm nav and desain home view

--- app/src/main/java/com/zn/zonaenglish/MainActivity.kt
+++ app/src/main/java/com/zn/zonaenglish/MainActivity.kt
@@ -2,12 +2,25 @@ package com.zn.zonaenglish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.zn.zonaenglish.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        /**
+         * Navigation with bottom nav
+         */
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }
